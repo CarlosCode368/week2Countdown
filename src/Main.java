a6cdc451d27212f9446d8409ca061c33fde32195
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-15 09:33:25 -0700

    Countdown

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int count = 10;
+        while (count >= 6) {//Evaluating the expression
+            System.out.println(count);
+            count--; //decrement count=count-1
+
+
         }
+
+        System.out.println("Checking ignition...");
+
+
+        int count2=5;
+        while(count2 >0) {//Evaluating the expression
+            System.out.println(count2);
+            count2--; //decrement count=count-1
+        }
+
+        System.out.println("BLAST OFF!!!");
+
+
     }
 }
